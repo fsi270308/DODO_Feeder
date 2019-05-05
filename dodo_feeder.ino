b9748f75e6f51e7f7938eaa1ae2bdb921529c4ec
MaF <fsi270308@gmail.com>
2019-05-05 12:56:08 +0800

    R1

--- dodo_feeder.ino
+++ dodo_feeder.ino
@@ -1,24 +1,44 @@
 /*
  * Maf,2019-4-22
  * DODO Feeder
+ * 
+ * A4988说明
+ * MS1,MS2,MS3跳线说明：（例子里是低电平，悬空或接地线，使用全步进模式）
+ * 分别是全步进，1/2步进，1/4步进，1/8步进，1/16步进模式。
+ * 步进电机走一步是1.8度，一圈就是200步。
+ * 例如使用1/16步进，则需要走3200步才等于一圈。
+ * 
+ * 2019-04-24
+ * 加载了StepperDriver库（https://github.com/laurb9/StepperDriver）
+ * A4988步进细分
+ *  MS1 - D10
+ *  MS2 - D11
+ *  MS3 - D12
+ *  
+ * 利用WIFI模块（https://s.click.taobao.com/NyAldBw）控制中间继电器吸合，
+ * 来触发动作。
  */
 
+#include <Arduino.h>
+#include "A4988.h"
+#define MOTOR_STEPS 200  //使用一圈200步的电机
+#define DIR 4
+#define STEP 5
+#define MS1 10
+#define MS2 11
+#define MS3 12
+A4988 stepper(MOTOR_STEPS, DIR, STEP, MS1, MS2, MS3);
 int x;
-int enable = 6;
-int sstep = 5;
-int dir = 4;
 int button = 7;
 boolean lastState = LOW;  //记录上次的开关状态，初始化为LOW
 boolean toggle = LOW;  //触发信号，初始化为LOW
 int bclick = 0;  //开关信号的改变次数，初始化为0
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(enable,OUTPUT);  //Enable
-  pinMode(sstep,OUTPUT);  //Step
-  pinMode(dir,OUTPUT);  //Dir
+  
+  stepper.begin(20, 16);  //设置电机转速为20rpm,细分为16
+  stepper.enable();
   pinMode(button,INPUT_PULLUP);  //按钮接内部上拉电阻
-  digitalWrite(enable,LOW);  //Set Enable low,6pin可不接  
   lastState = digitalRead(button);  //读取开关的初始状态值
 }
 
@@ -42,14 +62,7 @@ void loop() {
   {
     bclick = 0;  //归零
     toggle = !toggle;  //触发信号取相反值
-    digitalWrite(dir,HIGH);  //方向给定
-    for(x = 0; x < 200; x++)
-    {
-      digitalWrite(sstep,HIGH);
-      delayMicroseconds(800);
-      digitalWrite(sstep,LOW);
-      delayMicroseconds(800);
-    }
+    stepper.rotate(60/11.25*40);
     delay(1000);
   }
 }
